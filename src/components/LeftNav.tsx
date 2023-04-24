5fe83307d46aaa73a79437415ca40b3a9269ff17
siddharth <me@siddharthjain.in>
2023-04-24 19:29:07 +0530

    left nav abstracted

---
+++ src/components/LeftNav.tsx
@@ -0,0 +1,39 @@
+import { RouteList } from "@/helpers/constants";
+import React from "react";
+import readCSVFile from "../helpers/csvParser";
+import { useSpatialStore } from "@/store/store";
+
+function LeftNav() {
+  const setSpatialData = useSpatialStore((state) => state.setSpatialData);
+  const selectedRoute = useSpatialStore((state) => state.selectedRoute);
+  const setSelectedRoute = useSpatialStore((state) => state.setRoute);
+
+  function loadCSV(filepath: string) {
+    readCSVFile(filepath)
+      .then((rows) => {
+        setSelectedRoute(filepath);
+        setSpatialData(rows.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  return (
+    <div className="flex flex-col w-[20%] p-2  bg-white items-center  pt-6">
+      <div className="text-xl font-semibold text-zinc-600">
+        Select Bus Routes
+      </div>
+      {RouteList.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => loadCSV(item.filepath)}
+          className="p-2 text-xl text-red-500 underline cursor-pointer semibold"
+        >
+          {item.name}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default LeftNav;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,5 @@
 import dynamic from "next/dynamic";
 
-import { GTFSConfig, RouteList } from "../helpers/constants";
-
-import readCSVFile from "../helpers/csvParser";
-import { useSpatialStore } from "../store/store";
-
 import RouteTable from "../components/RouteTable";
 import Header from "../components/Header";
 
@@ -15,36 +10,8 @@ const MapComponent = dynamic(() => import("src/components/MapComponent.tsx"), {
 const isWindowContext = typeof window !== "undefined";
 
 export default function Home() {
-  const setSpatialData = useSpatialStore((state) => state.setSpatialData);
-  const selectedRoute = useSpatialStore((state) => state.selectedRoute);
-  const setSelectedRoute = useSpatialStore((state) => state.setRoute);
-  function loadCSV(filepath: string) {
-    readCSVFile(filepath)
-      .then((rows) => {
-        setSelectedRoute(filepath);
-        setSpatialData(rows.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
   return (
     <main className="flex flex-col min-h-screen">
-      {/* <div className="flex flex-col w-[20%] p-2  bg-white items-center  pt-6">
-        <div className="text-xl font-semibold text-zinc-600">
-          Select Bus Routes
-        </div>
-        {RouteList.map((item, index) => (
-          <div
-            key={index}
-            onClick={() => loadCSV(item.filepath)}
-            className="p-2 text-xl text-red-500 underline cursor-pointer semibold"
-          >
-            {item.name}
-          </div>
-        ))}
-      </div> */}
       <Header />
       <div className=" w-[90%] m-auto p-2 h-[calc(100vh- 40px)]">
         <MapComponent />
